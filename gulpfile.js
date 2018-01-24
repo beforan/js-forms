d447325db153edf729ce85d41ae4d6217b719591
Jonathan Couldridge <jcouldridge@outlook.com>
2018-01-24 10:17:13 +0000

    added browserify bits, changed gulp workflow to umd wrap, added `object.entries` shim

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-const gulp = require("gulp"),
-    babel = require("gulp-babel"),
-    del = require("del")
+const gulp = require('gulp');
+const browserify = require('browserify');
+const del = require('del');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
 
 //Chuck all Gulp built files
 gulp.task("clean", () => {
@@ -9,8 +11,17 @@ gulp.task("clean", () => {
 
 // compile and concatenate js
 gulp.task("js", () =>
-    gulp.src("src/js-forms.js")
-    .pipe(babel({ presets: [["env", { targets: { browsers: ["last 2 versions"] } }]] }))
-    .pipe(gulp.dest("dist")));
+browserify({
+    entries: './src/js-forms.js',
+    standalone: 'js-forms',
+  })
+  .transform('babelify', { presets: [['env', { targets: { browsers: ["last 2 versions"] } }]] })
+  .bundle()
+  .pipe(source('js-forms.js'))
+  .pipe(buffer())
+  .pipe(gulp.dest('dist')));
+    // gulp.src("src/js-forms.js")
+    // .pipe(babel({ presets: [["env", { targets: { browsers: ["last 2 versions"] } }]] }))
+    // .pipe(gulp.dest("dist")));
 
 gulp.task("default", ["clean", "js"]);
