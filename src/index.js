ca598d4fd38d78e0e93ca011ea0f68f10cd327c9
Jonathan Couldridge <jcouldridge@outlook.com>
2018-03-31 09:34:30 +0100

    moved private stuff and constants to their own files, added an index file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * @namespace JsForms
+ */
+
+ export * from './const';
---
+++ src/internal.js
@@ -0,0 +1,44 @@
+/*
+ * Internal Helper Methods
+ */
+
+/**
+ * Creates an HTML Form in memory, with named text inputs for the provided formData
+ * 
+ * @memberof JsForms
+ * @private
+ * @param {string} action The url to use as a Form Action
+ * @param {object} formData Keys/values to submit as named Form input fields
+ * @returns {HTMLFormElement} The created form
+ */
+export const createFormWithData = (action, formData) => {
+    //create a new hidden form to submit
+    const form = document.createElement("form");
+    form.action = action;
+    form.method = "post";
+    form.style.display = "none";
+
+    //add the extra values in naive inputs
+    for (let key in formData) {
+        if (!formData.hasOwnProperty(key)) continue;
+        const e = document.createElement("input");
+        e.name = key;
+        e.setAttribute("value", formData[key]);
+        form.appendChild(e);
+    }
+
+    return form;
+};
+
+/**
+ * Adds an in memory HTML Form to the DOM and immediately submits it
+ * 
+ * @memberof JsForms
+ * @private
+ * @param {HTMLFormElement} form 
+ */
+const submitForm = form => {
+    //add it to the DOM and submit
+    document.querySelector("body").appendChild(form);
+    form.submit();
+};
