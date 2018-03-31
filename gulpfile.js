527a406b001dd04c04dd2d8e7cb4d072cd9dc6b0
Jonathan Couldridge <jcouldridge@outlook.com>
2018-03-31 10:03:42 +0100

    npm clean tasks replacing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,7 @@
 const gulp = require('gulp');
 const browserify = require('browserify');
-const del = require('del');
 const buffer = require('vinyl-buffer');
 const source = require('vinyl-source-stream');
-
-//Chuck all Gulp built files
-gulp.task("clean", () => {
-    del.sync("dist/");
-});
 
 // compile and concatenate js
 gulp.task("js", () =>
